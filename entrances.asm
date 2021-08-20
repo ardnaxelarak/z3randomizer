a8e119285ed97a982f4b8fbdb92fe2b7e3bf4881
codemann8 <codemann8@gmail.com>
2021-08-19 21:53:26 -0500

    Actually fixed Mixed flag problems

--- entrances.asm
+++ entrances.asm
@@ -204,7 +204,7 @@ RTL
 AnimatedEntranceFix: ;when an entrance animation tries to start
 	PHA : PHX
 	LDA.l InvertedMode : BEQ + ;If we are in inverted mode
-		LDA.l OWMode+1 : AND.b #!FLAG_OW_MIXED : BEQ + ;If we are in Mixed OW shuffle mode
+		LDA.l OWMode+1 : AND.b #!FLAG_OW_MIXED : BNE + ;If we are in Mixed OW shuffle mode
 			LDA $8A : AND #$40 : BNE + ;and in the light world
 				PLX : PLA
 				STZ $04C6 ; skip it.
