7dabea9f8be47784c9053d810d2990801e4cca3f
codemann8 <codemann8@gmail.com>
2022-03-26 07:03:54 -0500

    Update events.asm

--- events.asm
+++ events.asm
@@ -154,7 +154,7 @@ OnNewFile:
 			LDA.b #$80 : STA $7EF093 ; open skull woods curtain
 		+
 
-		LDX.w #$007f : - ; copy over starting overworld event data
+		LDX.w #$007F : - ; copy over starting overworld event data
 			LDA StartingOverworldEvents, X : STA $7EF280, X
 			DEX
 		BPL -
