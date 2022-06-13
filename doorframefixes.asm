87cac1a113bd4e6129cb525317dd9778592ad8d4
Catobat <69204835+Catobat@users.noreply.github.com>
2022-06-13 20:37:59 +0200

    Shuffle Back of Tavern

--- doorframefixes.asm
+++ doorframefixes.asm
@@ -37,3 +37,41 @@ WalkDownIntoTavern:
 	CMP #$43
 RTL
 ;--------------------------------------------------------------------------------
+
+;--------------------------------------------------------------------------------
+; TurnAroundOnUnderworld
+;--------------------------------------------------------------------------------
+TurnAroundOnUnderworld:
+	LDA $26 : BEQ .done
+		; turn around if ($010E == #$43) != ($7F5099 == #$43)
+		LDX #$00
+		LDA #$43 : CMP $010E : BEQ +
+			INX
+		+
+		CMP $7F5099 : BEQ +
+			DEX
+		+
+		CPX #$00 : BEQ .done
+			LDA $26 : EOR #$0C : STA $26
+.done
+JML $0FFD65 ; what we overwrote
+
+;--------------------------------------------------------------------------------
+; TurnUpOnOverworld
+;--------------------------------------------------------------------------------
+TurnUpOnOverworld:
+	LDA.l EntranceTavernBack : CMP #$43 : BEQ .done
+		LDA #$08 : STA $26 ; only fix this glitch if exit not vanilla
+.done
+JML $07E68F ; what we overwrote
+
+;--------------------------------------------------------------------------------
+; WalkUpOnOverworld
+;--------------------------------------------------------------------------------
+WalkUpOnOverworld:
+	LDA $20 : CMP #$091B : BNE .normal ; hardcoded Y coordinate
+		STZ $2F
+		RTL
+	.normal
+	LDA #$0002 : STA $2F ; what we overwrote
+RTL
