23f839ccedea87832114639e3fd50082396340a4
aerinon <randall.rupper@gmail.com>
2024-01-26 11:33:32 -0700

    fix: swamola issue with cached sprites refactor: move some hooks

--- enemizer/blindboss.asm
+++ enemizer/blindboss.asm
@@ -2,7 +2,17 @@
 ; Blind Boss fight
 ;--------------------------------------------------------------------------------
 
-print "Blind Spawn Code Check: ", pc
+pushpc
+
+org $9DA081					; Original Code
+	JML check_blind_boss_room
+Check_for_Blind_Fight:
+
+org $9DA090
+Initialize_Blind_Fight:
+
+pullpc
+
 check_blind_boss_room:
 	LDA $A0                             ; load room index (low byte)
 	CMP #172 : BNE +                    ; Is is Thieves Town Boss Room
