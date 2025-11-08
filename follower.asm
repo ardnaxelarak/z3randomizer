f91e2eb1a2621bd0cbf32830f9abdb80c32455a0
codemann8 <codemann8@gmail.com>
2025-11-08 11:12:09 -0600

    Re-fixed purple chest follower dupe

--- follower.asm
+++ follower.asm
@@ -932,7 +932,8 @@ Locksmith_BecomeFollower:
 Locksmith_RespondToAnswer_PostItem:
     STA.l FollowerIndicator ; what we wrote over
     LDA.l FollowerTravelAllowed : CMP.b #$02 : BNE .no_despawn
-    LDA.w SpriteAux, X : BNE .no_despawn
+    LDA.w SpriteAux, X : CMP.b #$0C : BEQ .despawn
+        CMP.b #$00 : BNE .no_despawn
     LDA.l Follower_Locksmith : CMP.b #$0C : BEQ .despawn
     JSL DetermineFollowerSpawn_include_stored : BCC .no_despawn
 .despawn
